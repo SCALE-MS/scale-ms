d24ccebc7220a3c49d3627a474d71e2ce0dca3a0
M. Eric Irrgang <ericirrgang@gmail.com>
2021-03-04 17:58:48 +0300

    Consolidate rp.Session management in tests.
    Reuse rp.Session across pytest, where reasonable.
    
    Provide the event loop to WorkflowManager as required.

--- tests/test_rp_exec.py
+++ tests/test_rp_exec.py
@@ -69,142 +69,135 @@ def test_rp_usability():
             ...
 
 
-# ------------------------------------------------------------------------------
-#
-# Note: radical.pilot.Session creation causes several deprecation warnings.
-# Ref https://github.com/radical-cybertools/radical.pilot/issues/2185
-#
-@pytest.mark.filterwarnings('ignore::DeprecationWarning')
-@with_radical_only
-def test_rp_basic_task():
-
+@pytest.fixture(scope='session')
+def rpsession():
     import radical.pilot as rp
-
     # Note: Session creation will fail with a FileNotFound error unless venv
     #       is explicitly `activate`d (or the scripts installed with RADICAL components
     #       are otherwise made available on the PATH).
-    with rp.Session() as session:
 
-        # Based on `radical.pilot/examples/config.json`
-        # TODO: Does the Session have a default spec for 'local.localhost'?
-        #       Can/should we reference it?
-        #       https://github.com/radical-cybertools/radical.pilot/issues/2181
-        # NOTE: a session does not have a spec, really - the resource config
-        #       should be a *static* description of the target resource and
-        #       should not need any changing. (AM)
-        pd = rp.PilotDescription({'resource': 'local.localhost',
-                                  'cores'   : 32,
-                                  'gpus'    : 1})
-
-        td = rp.TaskDescription({'executable'   : '/bin/date',
-                                 'cpu_processes': 1})
-
-        pmgr  = rp.PilotManager(session=session)
-        tmgr  = rp.TaskManager(session=session)
-
-        pilot = pmgr.submit_pilots(pd)
-        task  = tmgr.submit_tasks(td)
-
-        tmgr.add_pilots(pilot)
-        tmgr.wait_tasks(uids=[task.uid])
-
-        assert task.exit_code == 0
-
+    # Note: radical.pilot.Session creation causes several deprecation warnings.
+    # Ref https://github.com/radical-cybertools/radical.pilot/issues/2185
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', category=DeprecationWarning)
+        session = rp.Session()
+    with session:
+        yield session
     assert session.closed
 
 
-# ------------------------------------------------------------------------------
-#
-# Note: radical.pilot.Session creation causes several deprecation warnings.
-# Ref https://github.com/radical-cybertools/radical.pilot/issues/2185
-#
-@pytest.mark.filterwarnings('ignore::DeprecationWarning')
 @with_radical_only
-def test_rp_scalems():
+def test_rp_basic_task(rpsession):
 
     import radical.pilot as rp
 
-    with rp.Session() as session:
+    # Based on `radical.pilot/examples/config.json`
+    # TODO: Does the Session have a default spec for 'local.localhost'?
+    #       Can/should we reference it?
+    #       https://github.com/radical-cybertools/radical.pilot/issues/2181
+    # NOTE: a session does not have a spec, really - the resource config
+    #       should be a *static* description of the target resource and
+    #       should not need any changing. (AM)
+    pd = rp.PilotDescription({'resource': 'local.localhost',
+                              'cores'   : 32,
+                              'gpus'    : 1})
 
-        # define a pilot and launch it
-        pd    = rp.PilotDescription(
-                   {'resource': 'local.localhost',
-                    'cores'   : 4,
-                    'gpus'    : 0})
+    td = rp.TaskDescription({'executable'   : '/bin/date',
+                             'cpu_processes': 1})
 
-        pmgr  = rp.PilotManager(session=session)
-        tmgr  = rp.TaskManager(session=session)
-        pilot = pmgr.submit_pilots(pd)
-        tmgr.add_pilots(pilot)
+    pmgr  = rp.PilotManager(session=rpsession)
+    tmgr  = rp.TaskManager(session=rpsession)
 
-        # TODO: How can we recover successful workflow stages from previous failed Sessions?
-        #
-        # The client needs to note the sandbox locations from runs. SCALEMS can
-        # then manage / maintain state tracking or state discovery to optimize workflow recovery.
-        # Resumed workflows can make reference to sandboxes from previous sessions
-        # (RCT work in progress: https://github.com/radical-cybertools/radical.pilot/tree/feature/sandboxes
-        # slated for merge in 2021 Q2 to support `sandbox://` URIs).
+    pilot = pmgr.submit_pilots(pd)
+    task  = tmgr.submit_tasks(td)
 
-        # define a raptor.scalems master and launch it within the pilot
-        pwd   = os.path.dirname(__file__)
-        td    = rp.TaskDescription(
-                {
-                    'uid'          :  'raptor.scalems',
-                    'executable'   :  'python3',
-                    'arguments'    : ['./scalems_test_master.py', '%s/scalems_test_cfg.json'  % pwd],
-                    'input_staging': ['%s/scalems_test_cfg.json'  % pwd,
-                                      '%s/scalems_test_master.py' % pwd,
-                                      '%s/scalems_test_worker.py' % pwd]
-                })
-        scheduler = tmgr.submit_tasks(td)
+    tmgr.add_pilots(pilot)
+    tmgr.wait_tasks(uids=[task.uid])
 
-        # define raptor.scalems tasks and submit them to the master
-        tds = list()
-        for i in range(2):
-            uid  = 'scalems.%06d' % i
-            # ------------------------------------------------------------------
-            # work serialization goes here
-            # This dictionary is interpreted by rp.raptor.Master.
-            work = json.dumps({'mode'      :  'call',
-                               'cores'     :  1,
-                               'timeout'   :  10,
-                               'data'      : {'method': 'hello',
-                                              'kwargs': {'world': uid}}})
-            # ------------------------------------------------------------------
-            tds.append(rp.TaskDescription({
-                               'uid'       : uid,
-                               'executable': 'scalems',  # This field is ignored by the ScaleMSMaster that receives this submission.
-                               'scheduler' : 'raptor.scalems', # 'scheduler' references the task implemented as a
-                               'arguments' : [work]  # Processed by raptor.Master._receive_tasks
-            }))
-
-        tasks = tmgr.submit_tasks(tds)
-        assert len(tasks) == len(tds)
-        # 'arguments' gets wrapped in a Request at the Master by _receive, then
-        # picked up by the Worker in _request_cb. Then picked up in forked interpreter
-        # by Worker._dispatch, which checks the *mode* of the Request and dispatches
-        # according to native or registered mode implementations. (e.g. 'call' (native) or 'scalems')
-
-        # task process is launched with Python multiprocessing (native) module and added to self._pool.
-        # When the task runs, it's result triggers _result_cb
-
-        # wait for *those* tasks to complete and report results
-        tmgr.wait_tasks(uids=[t.uid for t in tasks])
-
-        # Cancel the master.
-        tmgr.cancel_tasks(uids=scheduler.uid)
-        # Cancel blocks until the task is done so the following wait it currently redundant,
-        # but there is a ticket open to change this behavior.
-        # See https://github.com/radical-cybertools/radical.pilot/issues/2336
-        # tmgr.wait_tasks([scheduler.uid])
-
-        for t in tasks:
-            print('%s  %-10s : %s' % (t.uid, t.state, t.stdout))
-            assert t.state == rp.states.DONE
-            assert t.exit_code == 0
+    assert task.exit_code == 0
 
 
-    assert session.closed
+@with_radical_only
+def test_rp_scalems(rpsession):
+
+    import radical.pilot as rp
+
+    # define a pilot and launch it
+    pd    = rp.PilotDescription(
+               {'resource': 'local.localhost',
+                'cores'   : 4,
+                'gpus'    : 0})
+
+    pmgr  = rp.PilotManager(session=rpsession)
+    tmgr  = rp.TaskManager(session=rpsession)
+    pilot = pmgr.submit_pilots(pd)
+    tmgr.add_pilots(pilot)
+
+    # TODO: How can we recover successful workflow stages from previous failed Sessions?
+    #
+    # The client needs to note the sandbox locations from runs. SCALEMS can
+    # then manage / maintain state tracking or state discovery to optimize workflow recovery.
+    # Resumed workflows can make reference to sandboxes from previous sessions
+    # (RCT work in progress: https://github.com/radical-cybertools/radical.pilot/tree/feature/sandboxes
+    # slated for merge in 2021 Q2 to support `sandbox://` URIs).
+
+    # define a raptor.scalems master and launch it within the pilot
+    pwd   = os.path.dirname(__file__)
+    td    = rp.TaskDescription(
+            {
+                'uid'          :  'raptor.scalems',
+                'executable'   :  'python3',
+                'arguments'    : ['./scalems_test_master.py', '%s/scalems_test_cfg.json'  % pwd],
+                'input_staging': ['%s/scalems_test_cfg.json'  % pwd,
+                                  '%s/scalems_test_master.py' % pwd,
+                                  '%s/scalems_test_worker.py' % pwd]
+            })
+    scheduler = tmgr.submit_tasks(td)
+
+    # define raptor.scalems tasks and submit them to the master
+    tds = list()
+    for i in range(2):
+        uid  = 'scalems.%06d' % i
+        # ------------------------------------------------------------------
+        # work serialization goes here
+        # This dictionary is interpreted by rp.raptor.Master.
+        work = json.dumps({'mode'      :  'call',
+                           'cores'     :  1,
+                           'timeout'   :  10,
+                           'data'      : {'method': 'hello',
+                                          'kwargs': {'world': uid}}})
+        # ------------------------------------------------------------------
+        tds.append(rp.TaskDescription({
+                           'uid'       : uid,
+                           'executable': 'scalems',  # This field is ignored by the ScaleMSMaster that receives this submission.
+                           'scheduler' : 'raptor.scalems', # 'scheduler' references the task implemented as a
+                           'arguments' : [work]  # Processed by raptor.Master._receive_tasks
+        }))
+
+    tasks = tmgr.submit_tasks(tds)
+    assert len(tasks) == len(tds)
+    # 'arguments' gets wrapped in a Request at the Master by _receive, then
+    # picked up by the Worker in _request_cb. Then picked up in forked interpreter
+    # by Worker._dispatch, which checks the *mode* of the Request and dispatches
+    # according to native or registered mode implementations. (e.g. 'call' (native) or 'scalems')
+
+    # task process is launched with Python multiprocessing (native) module and added to self._pool.
+    # When the task runs, it's result triggers _result_cb
+
+    # wait for *those* tasks to complete and report results
+    tmgr.wait_tasks(uids=[t.uid for t in tasks])
+
+    # Cancel the master.
+    tmgr.cancel_tasks(uids=scheduler.uid)
+    # Cancel blocks until the task is done so the following wait it currently redundant,
+    # but there is a ticket open to change this behavior.
+    # See https://github.com/radical-cybertools/radical.pilot/issues/2336
+    # tmgr.wait_tasks([scheduler.uid])
+
+    for t in tasks:
+        print('%s  %-10s : %s' % (t.uid, t.state, t.stdout))
+        assert t.state == rp.states.DONE
+        assert t.exit_code == 0
 
 
 # ------------------------------------------------------------------------------
@@ -217,10 +210,11 @@ def test_rp_scalems():
 @with_radical_only
 async def test_exec_rp():
     original_context = scalems.context.get_context()
-    asyncio.get_event_loop().set_debug(True)
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
     logging.getLogger("asyncio").setLevel(logging.DEBUG)
     # Test RPDispatcher context
-    context = scalems.radical.RPWorkflowContext()
+    context = scalems.radical.RPWorkflowContext(loop)
     with scalems.context.scope(context):
         ...
         # TODO: re-enable test for basic executable wrapper.

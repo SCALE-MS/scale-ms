cfbe97c9ba2274a970103a8ec877d80fd0c5bce3
M. Eric Irrgang <ericirrgang@gmail.com>
2021-03-12 18:20:19 +0300

    Embrace PEP-517
    Ref https://setuptools.readthedocs.io/en/latest/build_meta.html

--- setup.py
+++ setup.py
@@ -1,34 +1,5 @@
 from setuptools import find_packages, setup
 
-setup(
-    name='scalems',
-    version='0',
-    # Python 3.8 incorporates some valuable typing helpers such as
-    # typing.Protocol (PEP 544), typing.TypedDict (PEP 589), and
-    # https://docs.python.org/3.8/library/functools.html#functools.singledispatchmethod.
-    # We require 3.8 for early development to reduce boiler-plate,
-    # back-ports, and workarounds for faster delivery.
-    # If necessary, we can back-port to Python 3.7.
-    python_requires='>=3.8',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    package_data={
-        # Enable PEP-561 style type hinting and .pyi type hinting files.
-        # Bundle some non-package scripts and config files.
-        'scalems': ['py.typed',
-                    'radical/data/*.json'],
-    },
-    entry_points={
-        'console_scripts': [
-            'scalems_rp_agent=scalems.radical.scalems_rp_agent:main',
-            'scalems_rp_worker=scalems.radical.scalems_rp_worker:main',
-        ],
-    },
-    # Somehow, this breaks the RP prepare_env:
-    install_requires=['radical.pilot@git+https://github.com/radical-cybertools/radical.pilot.git@project/scalems'],
-    url='https://github.com/SCALE-MS/scale-ms/',
-    license='',
-    author='SCALE-MS team',
-    author_email='',
-    description='SCALE-MS data flow scripting and graph execution for molecular science computational research protocols.'
-)
+# PEP-517 package details are in setup.cfg, but we keep this file for non-PEP-517 capable
+# installations, including "editable" installations with `pip install -e` or `setup.py develop`
+setup()

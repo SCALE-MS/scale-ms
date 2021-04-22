658a29a9ed2419d372d535cbf5633310191b9df8
M. Eric Irrgang <ericirrgang@gmail.com>
2021-04-22 11:44:30 +0300

    Fix logic error.

--- tests/test_rp_exec.py
+++ tests/test_rp_exec.py
@@ -156,6 +156,11 @@ def test_prepare_venv(rp_task_manager, sdist):
             'target': sdist_session_paths[name],
             'action': rp.TRANSFER
         })
+    pilot.stage_in(input_staging)
+
+    # Note: temporary check only works on localhost
+    for path in sdist_session_paths.values():
+        assert os.path.exists(path)
 
     tmgr = rp_task_manager
 

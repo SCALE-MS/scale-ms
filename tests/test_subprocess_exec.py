4cdf9132d1f323db1b806a4ff0cd3b5483898cd4
M. Eric Irrgang <ericirrgang@gmail.com>
2021-04-23 18:07:16 +0300

    Reduce cruft.

--- tests/test_subprocess_exec.py
+++ tests/test_subprocess_exec.py
@@ -11,40 +11,7 @@ import pytest
 
 import scalems.context
 import scalems.local
-import scalems.local_immediate
-from scalems.exceptions import MissingImplementationError
 from scalems.subprocess import executable
-
-
-# @pytest.mark.xfail(reason='There is currently no default executor. See also #53, #55, #82')
-# def test_exec_default():
-#     # Check for expected behavior of the default context
-#     with pytest.raises(MissingImplementationError):
-#         # Test default context
-#         cmd = executable(('/bin/echo',))
-#         context = scalems.context.get_context()
-#         # Note: we can choose to let Context.run(...) return a contextmanager that
-#         # can be used to create a session (if not already entered) that cleans up
-#         # after itself as best it can through a weakref from the context when the
-#         # reference count goes to zero, making the `with` block optional, but it
-#         # may be better to avoid too much automation or alternative ways to do things.
-#         with context as session:
-#             session.run(cmd)
-
-
-# This does not currently seem to be on the road map.
-# def test_exec_immediate():
-#     # Test immediate execution.
-#     context = scalems.local_immediate.ImmediateExecutionContext()
-#     with scalems.context.scope(context) as session:
-#         # TODO: Automatic context subscription so `cmd` can be obtained before non-default context.
-#         # Cross-context dispatching with the root context is not an important core feature now,
-#         # but it is a trivial test case for important cross-context dispatching in the near term,
-#         # and would be necessary for automated dispatching based on run-time environment, if we
-#         # were to pursue that in the future.
-#         cmd = scalems.executable(('/bin/echo',))
-#         # TODO: Check output
-#         session.run(cmd)
 
 
 @pytest.mark.asyncio

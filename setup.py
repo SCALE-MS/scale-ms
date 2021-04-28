7c12dadce8300d67faa68cabdc9222186c8c2602
M. Eric Irrgang <ericirrgang@gmail.com>
2021-04-28 20:05:09 +0300

    Generate and install sdist with install.
    Refs #204
    
    TODO:
    * Provide an interface to retrieve the installed data file,
      which has a version-dependent name.
    * Pre-configure the _version.py file in the sdist and make
      sure that it matches the archive name.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import build
 import os
+import pathlib
 import sys
 
 from setuptools import setup
@@ -18,6 +20,21 @@ if not _supported:
 sys.path.append(os.path.dirname(__file__))
 import versioneer
 
+from setuptools.command.build_py import build_py as _build_py
+
+
+_src_dir = pathlib.Path(__file__).parent
+
+
+class cmd_build_py(_build_py):
+    def build_package_data(self):
+        _build_py.build_package_data(self)
+        build_dir = os.path.join(self.build_lib, 'scalems', 'data')
+        dist = build.ProjectBuilder(str(_src_dir)).build(distribution='sdist', output_directory=build_dir)
+        print(f'Generated {dist}')
+
+
 # PEP-517 package details are in setup.cfg, but we keep this file for non-PEP-517 capable
 # installations, including "editable" installations with `pip install -e` or `setup.py develop`
-setup(cmdclass=versioneer.get_cmdclass())
+setup(cmdclass=versioneer.get_cmdclass({'build_py': cmd_build_py}))
+# setup(cmdclass=versioneer.get_cmdclass())

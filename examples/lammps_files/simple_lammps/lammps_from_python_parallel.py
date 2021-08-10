8b317200ce25052e2f650df0906b4a7f17d5cd34
Michael Shirts <michael.shirts@colorado.edu>
2021-08-10 12:23:14 -0600

    Adding parallel python example.

---
+++ examples/lammps_files/simple_lammps/lammps_from_python_parallel.py
@@ -0,0 +1,9 @@
+# run as  mpirun -np 4 python lammps_from_python_parallel.py
+from mpi4py import MPI
+from lammps import lammps
+lmp = lammps()
+lmp.file("in.lj")
+me = MPI.COMM_WORLD.Get_rank()
+nprocs = MPI.COMM_WORLD.Get_size()
+print("Proc %d out of %d procs has" % (me,nprocs),lmp)
+MPI.Finalize()

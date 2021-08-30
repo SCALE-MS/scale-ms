e03eeb232695bb223d4013d0245879645356a0d0
M. Eric Irrgang <ericirrgang@gmail.com>
2021-08-30 13:04:10 +0300

    Compartmentalize roles of `scalems.context`.

---
+++ src/scalems/context/_lock.py
@@ -0,0 +1,145 @@
+"""Provide locking protocol for scalems.context data stores."""
+
+__all__ = ['LockException', 'scoped_directory_lock']
+
+import contextlib
+import logging
+import os
+import typing
+import warnings
+
+logger = logging.getLogger(__name__)
+logger.debug('Importing {}'.format(__name__))
+
+_lock_directory_name = '.scalems_lock'
+"""Name to use for lock directories (module constant)"""
+
+
+class LockException(Exception):
+    """The requested lock could not be obtained."""
+
+
+def _lock_directory(path=None):
+    """Provide a fall-back mechanism for locking a directory.
+
+    If we find better ways to "lock" directories in certain environments, we can decide
+    how to dispatch alternative locking mechanisms. This implementation should be fairly
+    robust, though, and can always serve as a fall-back.
+
+    Locks must be explicitly removed by calling _unlock_directory(path). (Use
+    `scoped_directory_lock()` for a context manager.)
+
+    Note that, while this function should be thread-safe, the `_unlock_directory()`
+    function cannot be. Care should be taken not to call `_unlock_directory()` more than
+    once for a given token.
+
+    Raises:
+        LockException if the lock could not be obtained.
+
+    """
+    if path is None:
+        path = os.getcwd()
+    token_path = os.path.join(path, _lock_directory_name)
+    try:
+        os.mkdir(token_path)
+    except FileExistsError as e:
+        # Directory lock already exists.
+        raise LockException('{} already exists'.format(token_path)) from e
+    return token_path
+
+
+def _unlock_directory(path=None):
+    """Remove a lock from a directory path.
+
+    This must be called to release a lock established with _lock_directory(path).
+    """
+    if path is None:
+        path = os.getcwd()
+    token_path = os.path.join(path, _lock_directory_name)
+    assert os.path.exists(token_path)
+    os.rmdir(token_path)
+
+
+class _Lock:
+    """Represent a lock based on a filesystem token.
+
+    Takes ownership of a filesystem token.
+    The caller is responsible for calling `.release()`
+    exactly once to release the lock, removing the filesystem token.
+
+    If the caller has not explicitly released the Lock before it is destroyed,
+    a warning is issued and the __del__ method attempts to remove the object.
+    """
+    def __init__(self, filesystem_object=None):
+        if not os.path.exists(filesystem_object):
+            raise ValueError('Provided object is not a usable filesystem token.')
+        self._filesystem_object = filesystem_object
+
+    def is_active(self):
+        return self._filesystem_object is not None
+
+    def release(self):
+        if self._filesystem_object is None:
+            raise ValueError('Attempting to release an inactive lock.')
+        _Lock._remove(self._filesystem_object)
+        self._filesystem_object = None
+
+    @staticmethod
+    def _remove(path):
+        """Remove a filesystem object"""
+        if os.path.isfile(path):
+            os.unlink(path)
+        elif os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            warnings.warn('Cannot remove unknown filesystem object type: {}'.format(path))
+
+    def __del__(self):
+        if self._filesystem_object is not None:
+            warnings.warn('Lock object was not explicitly released! Token: {}'.format(self._filesystem_object))
+            obj = self._filesystem_object
+            self._filesystem_object = None
+            if os.path.exists(obj):
+                _Lock._remove(obj)
+
+
+@contextlib.contextmanager
+def scoped_directory_lock(path: typing.Union[str, bytes, os.PathLike] = None):
+    """Create a lock directory at the provided path or in the current directory.
+
+    Allows the caller to assert scoped control of a path through a filesystem
+    token instead of inter/intra-process communication.
+
+    Args:
+        path: an existing filesystem path.
+
+    Raises:
+         LockException if the lock directory could not be created.
+
+    Note:
+        Historically, lock directories can be more effective than lock files on shared
+        filesystems because of the atomicity of underlying filesystem commands. Feedback
+        welcome.
+
+    Caveats:
+        * No check is currently performed on subdirectories or parents of the path to be locked.
+        * The directory created as a lock token is assumed to remain empty.
+    """
+    if path is None:
+        path = os.getcwd()
+    token_path = _lock_directory(path)
+    try:
+        lock = _Lock(token_path)
+    except ValueError as e:
+        raise LockException('Could not create lock object.') from e
+    try:
+        yield lock
+        # Control returns to the last line of the `try` block if the the `with`
+        # block raises an exception.
+    finally:
+        # Note that unlink will fail if lock directory is not empty.
+        # This may be a bug or a feature. Time will tell. But we should be very
+        # careful about automatically removing non-empty directories on behalf
+        # of users.
+        if lock.is_active():
+            lock.release()
--- tests/test_context_datastore.py
+++ tests/test_context_datastore.py
@@ -1,4 +1,4 @@
-# Check that the basic Context implementation manages state as expected.
+"""Check that the basic Context implementation manages state as expected."""
 
 import contextlib
 import json
@@ -6,6 +6,8 @@ import os
 
 import pytest
 import scalems.context as _context
+import scalems.context._datastore
+import scalems.context._lock
 
 
 @contextlib.contextmanager
@@ -25,55 +27,55 @@ def test_normal_lifecycle(tmp_path):
     the current process already owns the data store.
     """
     with working_directory(tmp_path):
-        _context.get_context()
-        _context.finalize_context()
-        _context.get_context()
-        _context.get_context()
-        _context.finalize_context()
-        with pytest.raises(_context.StaleFileStore):
-            _context.finalize_context()
+        scalems.context._datastore.get_context()
+        scalems.context._datastore.finalize_context()
+        scalems.context._datastore.get_context()
+        scalems.context._datastore.get_context()
+        scalems.context._datastore.finalize_context()
+        with pytest.raises(scalems.context._datastore.StaleFileStore):
+            scalems.context._datastore.finalize_context()
 
 
 def test_nonfinalized(tmp_path):
     """Failure to call finalize_context() should have well-defined behavior."""
     with working_directory(tmp_path):
-        _context.get_context()
+        scalems.context._datastore.get_context()
         # In the future, more sophisticated tests might check that Singleton behavior or scoping
         # protections aren't violated.
         # Right now, we have to manipulate the filesystem with knowledge of the implementation
         # details in order to effectively test.
-        with open(_context._context_metadata_file, 'w') as fp:
+        with open(scalems.context._datastore._context_metadata_file, 'w') as fp:
             json.dump({'instance': 0}, fp)
-        with pytest.raises(_context.ContextError):
-            _context.get_context()
+        with pytest.raises(scalems.context._datastore.ContextError):
+            scalems.context._datastore.get_context()
         # We may want to assert constraints on the filesystem state we expect to encounter
         # even when a lock is left unexpectedly, but initially all we know is that a
         # dangling lock may result from an unclean process termination.
-        _context._lock_directory()
-        with pytest.raises(_context.ContextError):
-            _context.get_context()
-        _context._unlock_directory()
+        scalems.context._lock._lock_directory()
+        with pytest.raises(scalems.context._datastore.ContextError):
+            scalems.context._datastore.get_context()
+        scalems.context._lock._unlock_directory()
 
 
 def test_contention(tmp_path):
     """If two processes try to use the same data store, we should be able to
     detect and prevent it."""
     with working_directory(tmp_path):
-        _context.get_context()
-        _context._lock_directory()
-        with pytest.raises(_context.LockException):
-            _context.finalize_context()
-        _context._unlock_directory()
-        _context.finalize_context()
+        scalems.context._datastore.get_context()
+        scalems.context._lock._lock_directory()
+        with pytest.raises(scalems.context._lock.LockException):
+            scalems.context._datastore.finalize_context()
+        scalems.context._lock._unlock_directory()
+        scalems.context._datastore.finalize_context()
 
         expected_instance = os.getpid()
         unexpected_instance = expected_instance + 1
-        with open(_context._context_metadata_file, 'w') as fp:
+        with open(scalems.context._datastore._context_metadata_file, 'w') as fp:
             json.dump({'instance': unexpected_instance}, fp)
-        with pytest.raises(_context.ContextError):
-            _context.get_context()
+        with pytest.raises(scalems.context._datastore.ContextError):
+            scalems.context._datastore.get_context()
 
-        _context._lock_directory()
-        with pytest.raises(_context.ContextError):
-            _context.get_context()
-        _context._unlock_directory()
+        scalems.context._lock._lock_directory()
+        with pytest.raises(scalems.context._datastore.ContextError):
+            scalems.context._datastore.get_context()
+        scalems.context._lock._unlock_directory()

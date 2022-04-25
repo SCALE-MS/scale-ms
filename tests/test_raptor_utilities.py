458703a9b73f27e11af4a05d522a7d90464f8199
M. Eric Irrgang <ericirrgang@gmail.com>
2022-04-25 17:53:23 +0300

    Fix usage error.

--- tests/test_raptor_utilities.py
+++ tests/test_raptor_utilities.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 import packaging.version
 import pytest
@@ -37,7 +38,9 @@ def test_master():
     gpus_per_worker = 0
     # Note that the Worker launch has unspecified results if the `named_env`
     # does not exist and is not scheduled to be created with `prepare_env`.
+    _uid = uuid.uuid4()
     _worker_description = worker_description(
+        uid='scalems_worker.' + str(_uid),
         named_env='scalems_test_ve',
         cpu_processes=cores_per_worker,
         gpu_processes=gpus_per_worker,

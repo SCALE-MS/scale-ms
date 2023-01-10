52723a33d0fd657273d02c2b82657cd7c80d09d8
M. Eric Irrgang <ericirrgang@gmail.com>
2023-01-10 16:36:48 +0300

    Add a brief note about an assumption check.

--- tests/test_rp_exec.py
+++ tests/test_rp_exec.py
@@ -123,6 +123,9 @@ async def test_raptor_master(pilot_description, rp_venv, cleandir):
                 # Waiting longer doesn't seem to help.
                 # logger.debug("Waiting a little longer for the stop task to wrap up.")
                 # await asyncio.wait_for(stop_watcher, timeout=timeout)
+                # Note: if the following assertion ceases to be true, then we should
+                # adjust our assumptions about the behavior of tasks that include
+                # a `Master.stop()` in the result_cb.
                 assert stop_task.state not in rp.FINAL
             if not stop_watcher.done():
                 logger.debug(f"Canceling {stop_task}.")

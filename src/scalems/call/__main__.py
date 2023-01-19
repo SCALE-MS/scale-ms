cd69f83fd8ca06959a3638fedd7ca292723b06fe
M. Eric Irrgang <ericirrgang@gmail.com>
2023-01-19 18:31:13 +0300

    Add scalems.call module.
    Provide a command line entry point to execute a function according to a
    packaged representation of a function call, then package the result.
    
    Test the basic serializers and deserializers.

---
+++ src/scalems/call/__main__.py
@@ -0,0 +1,6 @@
+import sys
+
+import scalems.call
+
+if __name__ == "__main__":
+    sys.exit(scalems.call.cli(*sys.argv))
---
+++ tests/test_call.py
@@ -0,0 +1,58 @@
+"""Test our ability to call a packaged function call through our CLI handler.
+
+Example:
+    python -m scalems.call package.json
+"""
+import logging
+import os
+from subprocess import CompletedProcess
+from subprocess import run as subprocess_run
+import sys
+import tempfile
+
+import scalems.call
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+sample_call_input = dict(
+    func=subprocess_run,
+    kwargs={"args": ["/bin/echo", "hello", "world"], "capture_output": True},
+)
+
+
+def test_call_func():
+    """Test the API for scalems.call"""
+    call = scalems.call.pack_call(**sample_call_input)
+    result = scalems.call.main(scalems.call.unpack_call(call))
+    assert result.exception is None
+    complete_process: CompletedProcess = result.return_value
+    # Note that we are not treating OS text encoding generally here. However,
+    # this is a toy example in which we are just using `subprocess.run` as an
+    # arbitrary Python callable for testing. Let's not generalize unless it
+    # becomes and issue for devs in other locales.
+    assert "hello world" in complete_process.stdout.decode(encoding="utf8")
+
+
+def test_call_cli(tmpdir):
+    """Run the command line in a subprocess to confirm reasonable behavior."""
+    callpack = scalems.call.pack_call(**sample_call_input)
+    with tempfile.NamedTemporaryFile(suffix=".json", mode="w", dir=tmpdir) as tmp_file:
+        tmp_file.write(callpack)
+        tmp_file.flush()
+        pack_path = tmp_file.name
+        process = subprocess_run(
+            args=(sys.executable, "-m", "scalems.call", pack_path), capture_output=True, cwd=tmpdir
+        )
+    assert process.returncode == 0
+    outfile = os.path.join(tmpdir, "scalems_out.json")
+    assert os.path.exists(outfile)
+    with open(outfile, "r") as fh:
+        result: scalems.call.Result = scalems.call.unpack_result(fh.read())
+    completed_process: CompletedProcess = result.return_value
+    assert "hello world" in completed_process.stdout.decode(encoding="utf8")
+    # Output files need to be resolvable to local files.
+    # stdout = process.stdout
+    # stderr = process.stderr
+    # Confirm that expected output is contained (with the proper encoding)
